c25d52539fa6382f9605835599bfcbebccfef8b4
unwarysage <shawx538@morris.umn.edu>
2019-08-29 11:18:10 -0500

    The thing works

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -2,10 +2,16 @@
 
 #Charlot Shaw, Paul Friederichsen
 
-echo "You provided the arguments:" "$@"
+#echo "You provided the arguments:" "$@"
 
 NUMBER_ARGUMENT="$1" 
 PATH_ARGUMENT="$2"
 
 #Extract, untar, and decompress the file
-tar xvzf NthPrime.tgz -C $PATH_ARGUMENT
+tar xzf NthPrime.tgz -C $PATH_ARGUMENT
+
+#move into scratch
+cd $PATH_ARGUMENT/NthPrime
+
+gcc -o NthPrime main.c nth_prime.c
+./NthPrime $NUMBER_ARGUMENT
